4225184d0693b6ec6a084ca39849b40197552562
Patryk Kubacki <patryk.kubacki@outlook.com>
2018-03-04 17:24:39 +0100

    added architecture

---
+++ FiveSecondsClient.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiveSecondsClient.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult SetPlayer() => View();
+
+
+        //public async Task Index()
+        //{
+        //    HttpClient client = new HttpClient();
+        //    client.BaseAddress = new Uri("http://localhost:51371/api/Question");
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //        new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+
+        //    var x = response.Content.ReadAsStringAsync().Result;
+        //}
+    }
+}
